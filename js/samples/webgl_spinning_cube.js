798c4fb0a8196b977a544fa66ec94068fc1da820
Dimitri Roche <dimroc@gmail.com>
2012-09-07 08:25:38 -0400

    Paste WebGL spinning cube sample verbatim(broken)

--- js/samples/webgl_spinning_cube.js
+++ js/samples/webgl_spinning_cube.js
@@ -1,34 +1,171 @@
 (function() {
 
+  // Slightly modified version of http://www.khronos.org/webgl/wiki/Tutorial
+
+  function init(scene) {
+    // Initialize
+    var gl = WebGLUtils.setupWebGL(scene.canvas);
+    if (!gl) {
+      return;
+    }
+
+    scene.program = simpleSetup(
+      gl, "vshader", "fshader",
+      [ "vNormal", "vColor", "vPosition"], [ 0, 0, 0, 1 ], 10000
+    );
+
+    // Set some uniform variables for the shaders
+    gl.uniform3f(gl.getUniformLocation(scene.program, "lightDir"), 0, 0, 1);
+    gl.uniform1i(gl.getUniformLocation(scene.program, "sampler2d"), 0);
+
+    // Create a box. with the BufferObjects containing the arrays
+    // for vertices, normals, texture coords, and indices.
+    scene.box = makeBox(gl);
+
+    // Set up the array of colors for the cube's faces
+    var colors = new Uint8Array(
+      [  0, 0, 1, 1,   0, 0, 1, 1,   0, 0, 1, 1,   0, 0, 1, 1,     // v0-v1-v2-v3 front
+         1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,     // v0-v3-v4-v5 right
+         0, 1, 0, 1,   0, 1, 0, 1,   0, 1, 0, 1,   0, 1, 0, 1,     // v0-v5-v6-v1 top
+         1, 1, 0, 1,   1, 1, 0, 1,   1, 1, 0, 1,   1, 1, 0, 1,     // v1-v6-v7-v2 left
+         1, 0, 1, 1,   1, 0, 1, 1,   1, 0, 1, 1,   1, 0, 1, 1,     // v7-v4-v3-v2 bottom
+         0, 1, 1, 1,   0, 1, 1, 1,   0, 1, 1, 1,   0, 1, 1, 1 ]    // v4-v7-v6-v5 back
+    );
+
+    // Set up the vertex buffer for the colors
+    scene.box.colorObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.box.colorObject);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    // Create some matrices to use later and save their locations in the shaders
+    scene.mvMatrix = new J3DIMatrix4();
+    scene.u_normalMatrixLoc = gl.getUniformLocation(scene.program, "u_normalMatrix");
+    scene.normalMatrix = new J3DIMatrix4();
+    scene.u_modelViewProjMatrixLoc = gl.getUniformLocation(scene.program, "u_modelViewProjMatrix");
+    scene.mvpMatrix = new J3DIMatrix4();
+
+    // Enable all of the vertex attribute arrays.
+    gl.enableVertexAttribArray(0);
+    gl.enableVertexAttribArray(1);
+    gl.enableVertexAttribArray(2);
+
+    // Set up all the vertex attributes for vertices, normals and texCoords
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.box.vertexObject);
+    gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.box.normalObject);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.box.texCoordObject);
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
+
+    // Bind the index array
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scene.box.indexObject);
+
+    return gl;
+  }
+
+  function reshape(scene, gl) {
+    var canvas = scene.canvas;
+
+    // if the display size of the canvas has changed
+    // change the size we render at to match.
+    // if (canvas.clientWidth == canvas.width && canvas.clientHeight == canvas.height) {
+      // return;
+    // }
+
+    // canvas.width = canvas.clientWidth;
+    // canvas.height = canvas.clientHeight;
+
+    // Set the viewport and projection matrix for the scene
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    scene.perspectiveMatrix = new J3DIMatrix4();
+    scene.perspectiveMatrix.lookat(0, 0, 7, 0, 0, 0, 0, 1, 0);
+    scene.perspectiveMatrix.perspective(30, canvas.width / canvas.height, 1, 10000);
+  }
+
+  function drawPicture(scene, gl) {
+    //Make sure the canvas is sized correctly.
+    reshape(scene, gl);
+
+    // Clear the canvas
+    gl.clearColor(0,0,0,0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Make a model/view matrix.
+    scene.mvMatrix.makeIdentity();
+    scene.mvMatrix.rotate(20, 1, 0, 0);
+    scene.mvMatrix.rotate(scene.currentAngle, 0, 1, 0);
+
+    // Construct the normal matrix from the model-view matrix and pass it in
+    scene.normalMatrix.load(scene.mvMatrix);
+    scene.normalMatrix.invert();
+    scene.normalMatrix.transpose();
+    scene.normalMatrix.setUniform(gl, scene.u_normalMatrixLoc, false);
+
+    // Construct the model-view * projection matrix and pass it in
+    scene.mvpMatrix.load(scene.perspectiveMatrix);
+    scene.mvpMatrix.multiply(scene.mvMatrix);
+    scene.mvpMatrix.setUniform(gl, gl.u_modelViewProjMatrixLoc, false);
+
+    // Bind the texture to use
+    // gl.bindTexture(gl.TEXTURE_2D, spiritTexture);
+
+    // Draw the cube
+    gl.drawElements(gl.TRIANGLES, scene.box.numIndices, gl.UNSIGNED_BYTE, 0);
+
+    // Show the framerate
+    // framerate.snapshot();
+
+    scene.currentAngle += scene.incAngle;
+    if (scene.currentAngle > 360) {
+      scene.currentAngle -= 360;
+    }
+  }
+
   window.samples.webgl_spinning_cube = {
 
     initialize: function(canvas) {
-      var scene = new THREE.Scene();
+      //canvas = WebGLDebugUtils.makeLostContextSimulatingCanvas(canvas);
+      // tell the simulator when to lose context.
+      //canvas.loseContextInNCalls(15);
 
-      var camera = new THREE.PerspectiveCamera( 75, sample_defaults.width / sample_defaults.height, 1, 1000 );
-      camera.position.z = 100;
+      var scene = { canvas: canvas };
 
-      var geometry = new THREE.CubeGeometry( 70, 70, 70 );
-      var material = new THREE.MeshLambertMaterial( { color: 0xdddddd } );
+      canvas.addEventListener('webglcontextlost', handleContextLost, false);
+      canvas.addEventListener('webglcontextrestored', handleContextRestored, false);
 
-      var mesh = new THREE.Mesh( geometry, material );
-      scene.add( mesh );
+      canvas.width = sample_defaults.width;
+      canvas.height = sample_defaults.height;
 
-      var renderer = new THREE.CanvasRenderer({canvas: canvas});
-      renderer.setSize( sample_defaults.width, sample_defaults.height );
+      var gl = init(scene);
+      if (!gl) {
+        return;
+      }
+
+      scene.currentAngle = 0;
+      scene.incAngle = 0.5;
+      // framerate = new Framerate("framerate");
 
       function animate() {
-
-        // note: three.js includes requestAnimationFrame shim
-        requestAnimationFrame( animate );
-
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
-
-        renderer.render( scene, camera );
+        drawPicture(scene, gl);
+        requestId = window.requestAnimFrame(animate, canvas);
       }
 
       animate();
+
+      function handleContextLost(e) {
+        e.preventDefault();
+        if (requestId !== undefined) {
+          window.cancelAnimFrame(requestId);
+          requestId = undefined;
+        }
+      }
+
+      function handleContextRestored() {
+        init(scene);
+        animate();
+      }
     }
   };
 })();
