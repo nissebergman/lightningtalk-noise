b6ef7829689ce862ec5bd6102c5dfc604d9d1697
Dimitri Roche <dimroc@gmail.com>
2012-09-11 07:58:07 -0400

    Complete particle section

--- js/samples/particles.js
+++ js/samples/particles.js
@@ -1,32 +1,83 @@
 (function() {
 
+  var container;
+  var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color;
+  var mouseX = 0, mouseY = 0;
+
+  var width = sample_defaults.width * 2;
+  var height = sample_defaults.height * 2;
+
+  function animate() {
+    requestAnimationFrame( animate );
+    render();
+  }
+
+  function render() {
+    var time = Date.now() * 0.00005;
+
+    for ( i = 0; i < scene.children.length; i ++ ) {
+      var object = scene.children[ i ];
+
+      if ( object instanceof THREE.ParticleSystem ) {
+        object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+      }
+    }
+
+    for ( i = 0; i < materials.length; i ++ ) {
+      color = parameters[i][0];
+
+      h = ( 360 * ( color[0] + time ) % 360 ) / 360;
+      materials[i].color.setHSV( h, color[1], color[2] );
+    }
+
+    renderer.render( scene, camera );
+  }
+
   window.samples.particles = {
 
     initialize: function(canvas) {
-      var scene = new THREE.Scene();
 
-      var camera = new THREE.PerspectiveCamera( 75, sample_defaults.width / sample_defaults.height, 1, 1000 );
-      camera.position.z = 100;
+      camera = new THREE.PerspectiveCamera( 75, width / height, 1, 3000 );
+      camera.position.z = 1000;
 
-      var geometry = new THREE.CubeGeometry( 70, 70, 70 );
-      var material = new THREE.MeshLambertMaterial( { color: 0xdddddd } );
+      scene = new THREE.Scene();
+      scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
 
-      var mesh = new THREE.Mesh( geometry, material );
-      scene.add( mesh );
+      geometry = new THREE.Geometry();
 
-      var renderer = new THREE.CanvasRenderer({canvas: canvas});
-      renderer.setSize( sample_defaults.width, sample_defaults.height );
+      for ( i = 0; i < 20000; i ++ ) {
 
-      function animate() {
+        var vertex = new THREE.Vector3();
+        vertex.x = Math.random() * 2000 - 1000;
+        vertex.y = Math.random() * 2000 - 1000;
+        vertex.z = Math.random() * 2000 - 1000;
 
-        // note: three.js includes requestAnimationFrame shim
-        requestId = requestAnimationFrame( animate );
-
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
-
-        renderer.render( scene, camera );
+        geometry.vertices.push( vertex );
       }
+
+      parameters = [ [ [1.0, 1.0, 1.0], 5 ], [ [0.95, 1, 1], 4 ], [ [0.90, 1, 1], 3 ], [ [0.85, 1, 1], 2 ], [ [0.80, 1, 1], 1 ] ];
+
+      for ( i = 0; i < parameters.length; i ++ ) {
+
+        size  = parameters[i][1];
+        color = parameters[i][0];
+
+        //materials[i] = new THREE.ParticleBasicMaterial( { color: color, size: size } );
+
+        materials[i] = new THREE.ParticleBasicMaterial( { size: size } );
+        materials[i].color.setHSV( color[0], color[1], color[2] );
+
+        particles = new THREE.ParticleSystem( geometry, materials[i] );
+
+        particles.rotation.x = Math.random() * 6;
+        particles.rotation.y = Math.random() * 6;
+        particles.rotation.z = Math.random() * 6;
+
+        scene.add( particles );
+      }
+
+      renderer = new THREE.WebGLRenderer({canvas: canvas});
+      renderer.setSize( width, height );
 
       animate();
     }
