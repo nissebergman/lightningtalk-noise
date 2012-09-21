6456ab1f82f61c0b25eb02bf2ce27e67151b0776
Dimitri Roche <dimroc@gmail.com>
2012-09-21 08:51:10 -0400

    Update Lighting section and lighted_sphere.js

--- js/samples/lighted_sphere.js
+++ js/samples/lighted_sphere.js
@@ -1,30 +1,39 @@
 (function() {
 
+  var width, height;
+  width = height = 300;
+
   window.samples.lighted_sphere = {
 
     initialize: function(canvas) {
       var scene = new THREE.Scene();
 
-      var camera = new THREE.PerspectiveCamera( 75, sample_defaults.width / sample_defaults.height, 1, 1000 );
+      var camera = new THREE.PerspectiveCamera( 75, width / height, 1, 1000 );
       camera.position.z = 100;
 
-      var geometry = new THREE.CubeGeometry( 70, 70, 70 );
+      var geometry = new THREE.SphereGeometry( 30, 30, 30 );
       var material = new THREE.MeshLambertMaterial( { color: 0xdddddd } );
 
       var mesh = new THREE.Mesh( geometry, material );
       scene.add( mesh );
 
+      var spotLight = new THREE.SpotLight ( 0xffffffff );
+      spotLight.position.set( 0, 100, 0 );
+      scene.add( spotLight );
+
       var renderer = new THREE.WebGLRenderer({canvas: canvas});
-      renderer.setSize( sample_defaults.width * 0.5, sample_defaults.height * 0.5);
+      renderer.setSize( width, height);
 
       var instance = { active: false };
       function animate() {
         requestAnimationFrame( animate, canvas );
         if(!instance.active || sample_defaults.paused) return;
 
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
+        // Rotate lighting around the X axis.
+        var angle = 0.01;
+        var matrix = new THREE.Matrix4().makeRotationX( angle );
 
+        spotLight.position = matrix.multiplyVector3( spotLight.position );
         renderer.render( scene, camera );
       }
 
